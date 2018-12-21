b9c78cc7f23b2dc0479132835b16bd532947fc88
linguangjie <373763470@qq.com>
2018-12-21 15:44:30 +0800

    修改

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="page">
       <home-header></home-header>
+      <home-swiper></home-swiper>
+      <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import homeHeader from '../components/home/Header'
+import homeSwiper from '../components/home/Swiper'
+import homeIcons from '../components/home/Icons'
 export default {
   name: "Home",
   data() {
     return {};
   },
   components: {
-      homeHeader
+      homeHeader,
+      homeSwiper,
+      homeIcons
   }
 };
 </script>
