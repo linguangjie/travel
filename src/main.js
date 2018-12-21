783f930626b95b1cd7a093018bc40c5e2255f602
linguangjie <373763470@qq.com>
2018-12-21 14:44:34 +0800

    添加了头部的组件

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import router from './router'
 import store from './store'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
 import fastClick from 'fastclick'
 Vue.config.productionTip = false
 
 fastClick.attach(document.body)
+
 new Vue({
   router,
   store,
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="page">首页</div>
+  <div class="page">
+      <home-header></home-header>
+  </div>
 </template>
 
 <script>
+import homeHeader from '../components/home/Header'
 export default {
   name: "Home",
   data() {
     return {};
   },
-  components: {}
+  components: {
+      homeHeader
+  }
 };
 </script>
 
