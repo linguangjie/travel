ad7a96b1ac35a3fc1889e089e935a2dd1d5c1e86
linguangjie <373763470@qq.com>
2018-12-21 10:18:49 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ import router from './router'
 import store from './store'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
-
+import fastClick from 'fastclick'
 Vue.config.productionTip = false
 
+fastClick.attach(document.body)
 new Vue({
   router,
   store,
