13c391e8d546e1bb03f3853b7e5a7974f9d64d1a
linguangjie <373763470@qq.com>
2018-12-21 22:19:15 +0800

    add icons

--- src/components/home/Icons.vue
+++ src/components/home/Icons.vue
@@ -1,16 +1,87 @@
 <template>
-  <div class="page"></div>
+  <div class="icons">
+    <swiper :options="swiperOption" v-for="(page, index) in pages" :key="index">
+      <swiper-slide>
+        <div class="icon" v-for="(item,index) in page" :key="index">
+          <div class="icon-img">
+            <img :src="item" class="icon-img-content">
+          </div>
+          <p class="icon-desc">{{item}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
 </template>
 
 <script>
 export default {
   name: "homeIcons",
   data() {
-    return {};
+    return {
+      iconList: []
+    };
+  },
+  computed: {
+    pages() {
+      const pages = [];
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8);
+        if (!pages[page]) {
+          pages[page] = [];
+        }
+        pages[page].push(item);
+      });
+      return pages;
+    }
   },
   components: {}
 };
 </script>
 
 <style scoped lang="stylus">
+@import '../../assets/styles/varibles.styl';
+@import '../../assets/styles/mixins.styl';
+
+.icons >>> .swiper-container {
+  height: 0;
+  padding-bottom: 50%;
+}
+
+.icon {
+  position: relative;
+  overflow: hidden;
+  float: left;
+  width: 25%;
+  padding-bottom: 25%;
+  background-color: red;
+
+  .icon-img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0.44rem;
+    box-sizing: border-box;
+    padding: 0.1rem;
+    background-color: blue;
+
+    .icon-img-content {
+      display: block;
+      margin: 0 auto;
+      height: 100%;
+    }
+  }
+
+  .icon-desc {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 0.44rem;
+    line-height: 0.44rem;
+    text-align: center;
+    color: $darkTextColor;
+    ellipsis();
+  }
+}
 </style>
