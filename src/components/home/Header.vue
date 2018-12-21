10fd2f5c0cc2daf3d62eb405ec979b2aa75e8deb
linguangjie <373763470@qq.com>
2018-12-21 15:28:28 +0800

    change

--- src/components/home/Header.vue
+++ src/components/home/Header.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="header">
     <div class="header-left">
-        <div class="iconfont back-icon">&#xe624;</div>
+      <div class="iconfont back-icon">&#xe624;</div>
     </div>
     <div class="header-input">
-        <span class="iconfont">&#xe632;</span>
-        输入城市/景点/游玩主题</div>
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
     <div class="header-right">
-        城市<span class="iconfont arrow-icon">&#xe64a;</span>
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    name:'homeHeader',
+  name: "homeHeader",
   data() {
     return {};
   },
@@ -23,34 +25,46 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-    @import '../../assets/styles/varibles.styl'
-    .header
-        display :flex
-        line-height :.86rem
-        background-color :$bgColor
-        color :#fff
-        .header-left:
-            width :.64rem
-            float: left
-            .back-icon
-                text-align : center
-                font-size : .4rem
-        .header-input
-            flex:1
-            line-height :.64rem
-            height :.64rem
-            margin-top :.12rem
-            margin-left :.2rem
-            padding-left :.2rem
-            background-color :#fff
-            border-radius :.1rem
-            color:#ccc
-        .header-right
-          width :1.24rem
-          float:right
-          text-align :center
-          .arrow-icon
-            margin-left: -.04rem
-            font-size: .24rem 
+@import '../../assets/styles/varibles.styl';
+
+.header {
+  display: flex;
+  line-height: 0.86rem;
+  background-color: $bgColor;
+  color: #fff;
+
+  .header-left {
+    width: 0.64rem;
+    float: left;
+
+    .back-icon {
+      text-align: center;
+      font-size: 0.4rem;
+    }
+  }
+
+  .header-input {
+    flex: 1;
+    line-height: 0.64rem;
+    height: 0.64rem;
+    margin-top: 0.12rem;
+    margin-left: 0.2rem;
+    padding-left: 0.2rem;
+    background-color: #fff;
+    border-radius: 0.1rem;
+    color: #ccc;
+  }
+
+  .header-right {
+    width: 1.24rem;
+    float: right;
+    text-align: center;
+
+    .arrow-icon {
+      margin-left: -0.04rem;
+      font-size: 0.24rem;
+    }
+  }
+}
 </style>
 
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="page">
       <home-header></home-header>
+      <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 import homeHeader from '../components/home/Header'
+import HomeSwiper from '../components/home/Swiper'
 export default {
   name: "Home",
   data() {
     return {};
   },
   components: {
-      homeHeader
+      homeHeader,
+      HomeSwiper
   }
 };
 </script>
