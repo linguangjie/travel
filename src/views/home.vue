992d92634896633c485557743e3e1da3fedf8ff5
linguangjie <373763470@qq.com>
2018-12-21 09:17:17 +0800

    first commit

---
+++ src/views/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page">首页</div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {};
+  },
+  components: {}
+};
+</script>
+
+<style scoped>
+</style>
